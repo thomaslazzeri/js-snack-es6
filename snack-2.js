6e5a1d1765432a95c34cf1bfc36dfb753c29e4c4
thomaslazzeri3 <thomaslazzeri3@gmail.com>
2026-06-06 20:34:41 +0200

    changed html for exercise snack-2 and add variable squads with objects

---
+++ snack-2.js
@@ -0,0 +1,18 @@
+const squads = [
+    {
+        name: 'Juventus',
+        pointsScored: 0,
+        FoulsDrawn: 0,
+    },
+    {
+        name: 'Milan',
+        pointsScored: 0,
+        FoulsDrawn: 0,
+    },
+    {
+        name: 'Inter',
+        pointsScored: 0,
+        FoulsDrawn: 0,
+    },
+];
+
